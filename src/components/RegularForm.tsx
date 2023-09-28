698b71b7c1900f08b044cf8f4bf6f65d8dd85675
ari shtipi <arish186777@gmail.com>
2023-09-28 12:09:58 +0300

    ErrorMessage  from  React Hook Form

--- src/components/RegularForm.tsx
+++ src/components/RegularForm.tsx
@@ -1,56 +1,76 @@
 import { useForm, SubmitHandler } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
 
 interface FormData {
   username: string;
   email: string;
   password: string;
 }
-const passwordPattern = /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[@#$%^&+=!])(?!.*\s).{8,20}$/;
+
+const passwordPattern: RegExp =
+  /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[@#$%^&+=!])(?!.*\s).{8,20}$/;
+const emailPattern: RegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 const onSubmit: SubmitHandler<FormData> = (data) =>
-  alert(` user name :${data.username} email: ${data.email} `);
+  alert(`Username: ${data.username}, Email: ${data.email}`);
 
 function RegularForm() {
   const {
     register,
     formState: { errors },
     handleSubmit,
-  } = useForm<FormData>();
+  } = useForm<FormData>({
+    criteriaMode: "all",
+  });
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <h1>React Hook Form</h1>
       <div>
         <input
-          placeholder="enter user name"
+          placeholder="Enter username"
           {...register("username", {
-            required: true,
-            maxLength: 20,
-            minLength: 2,
+            required: "Username is required.",
+            maxLength: {
+              value: 20,
+              message: "Username must not exceed 20 characters.",
+            },
+            minLength: {
+              value: 2,
+              message: "Username must be at least 2 characters.",
+            },
           })}
         />
-        {errors.username && "username is required"}
+        <ErrorMessage errors={errors} name="username" as="p" />
       </div>
       <div>
         <input
-          type="email"
-          placeholder="enter email"
+          placeholder="Enter email"
           {...register("email", {
-            required: true,
-            minLength: 7,
+            required: "Email is required.",
+            pattern: {
+              value: emailPattern,
+              message: "Invalid email address.",
+            },
           })}
         />
+        <ErrorMessage errors={errors} name="email" as="p" />
       </div>
       <div>
         <input
-          placeholder="enter password"
+          type="password"
+          placeholder="Enter password"
           {...register("password", {
-            required: true,
-            pattern: passwordPattern,
+            required: "Password is required.",
+            pattern: {
+              value: passwordPattern,
+              message: "Invalid password.",
+            },
           })}
         />
-        {errors.password && "password is required"}
+        <ErrorMessage errors={errors} name="password" as="p" />
       </div>
+
       <input type="submit" />
     </form>
   );
